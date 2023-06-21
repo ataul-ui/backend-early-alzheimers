f436940634edef3d03f061a2c28d13f12132a8ea
Ataulhaq Akbar <ataul.akbar@gmail.com>
2023-06-21 17:03:09 -0400

    made necessary adjustments to other tasks, ready to write the github actions task now

--- dags/eye_dag.py
+++ dags/eye_dag.py
@@ -32,14 +32,13 @@ def score_data(**kwargs):
         raise Exception('No data.')
     else:
         return rest
-
-
-def upload_to_postgre_eye_data():
-    cwd = os.getcwd()
-    file_path = os.path.join(cwd, "dags", "data", "ocular.json")
     
-    with open(file_path, "r") as json_file:
-        json_data = json.load(json_file)
+def upload_to_postgre_speech_data(**kwargs):
+    cwd = os.getcwd()
+    file_path_speech = os.path.join(cwd, "dags", "data", "speech_data.json")
+    
+    with open(file_path_speech, "r") as json_file_speech:
+        json_data_speech = json.load(json_file_speech)
     
     conn = psycopg2.connect(
         host=host,
@@ -49,13 +48,52 @@ def upload_to_postgre_eye_data():
         password=password
     )
     cur = conn.cursor()
-    cur.execute("INSERT INTO user_info (biomarker) VALUES (%s)", (json_data["biomarker"],))
+    cur.execute("INSERT INTO user_info (score) VALUES (%s)", (json_data_speech["score"],))
     
     conn.commit()
     cur.close()
     conn.close()
     
+    context = kwargs
+    context['ti'].xcom_push(key='speechData', value='speech data has been received')
+    
+    return "uploaded speech data"
+
+
+def upload_to_postgre_eye_data(**kwargs):
+    cwd = os.getcwd()
+    file_path_eye = os.path.join(cwd, "dags", "data", "ocular.json")
+    
+    with open(file_path_eye, "r") as json_file_eye:
+        json_data_eye = json.load(json_file_eye)
+    
+    conn = psycopg2.connect(
+        host=host,
+        port=port,
+        dbname=dbname,
+        user=user,
+        password=password
+    )
+    cur = conn.cursor()
+    cur.execute("INSERT INTO user_info (biomarker) VALUES (%s)", (json_data_eye["biomarker"],))
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+    context = kwargs
+    context['ti'].xcom_push(key='eyeData', value='ocular data has been received')
+    
     return "will_upload"
+
+def confirm_data_upload(**kwargs):
+    ti = kwargs['ti']
+    recieved_speech = ti.xcom_pull(key='speechData')
+    recieved_eye = ti.xcom_pull(key='eyeData')
+    
+    
+    print(recieved_speech)
+    print(recieved_eye)
 
 
 with DAG("eye_pipeline", start_date=datetime(2021,1,1),
@@ -75,9 +113,25 @@ with DAG("eye_pipeline", start_date=datetime(2021,1,1),
         provide_context=True
     )
     
-    database_upload = PythonOperator(
-        task_id='database_upload',
-        python_callable=upload_to_postgre_eye_data
+    database_upload_eye = PythonOperator(
+        task_id='database_upload_eye',
+        python_callable=upload_to_postgre_eye_data,
+        do_xcom_push=True,
+        provide_context=True
     )
     
-    load_file >> scored_result >> database_upload
+    database_upload_speech = PythonOperator(
+        task_id='database_upload_speech',
+        python_callable=upload_to_postgre_speech_data,
+        do_xcom_push=True,
+        provide_context=True
+    )
+    
+    confirmation_task = PythonOperator(
+        task_id='confirmation_task',
+        python_callable=confirm_data_upload,
+        do_xcom_push=True,
+        provide_context=True
+    )
+    
+    [database_upload_eye, database_upload_speech] >> confirmation_task
