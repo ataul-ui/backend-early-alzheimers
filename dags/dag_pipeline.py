63f216e6a4b05bff2170cf96fd93ca5984e58aa3
Ataulhaq Akbar <ataul.akbar@gmail.com>
2023-06-21 17:46:08 -0400

    load_dotenv works properly

--- dags/dag_pipeline.py
+++ dags/dag_pipeline.py
@@ -8,11 +8,13 @@ from airflow.operators.python_operator import PythonOperator
 
 #LET THERE ONLY BE ONE DAG FI.E, AND IT SHOULD BE THIS ONE
 #COMBINE IT WITH SPEECH_DAY.py
+load_dotenv()
+var1 = os.getenv("dbname")
 
 # Define database credentials
 host = 'host.docker.internal'
 port = '5432'
-dbname = 'airflow'
+dbname = var1
 user = 'airflow'
 password = 'airflow'
     
